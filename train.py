987abcd3dc50f3d1aecb8056299b613da6b8df70
yiang.zhu <yiang.zhu@sohan.hk>
2019-01-23 11:18:19 +0800

    fixed the bug of raising Error when the first epoch finished.

--- train.py
+++ train.py
@@ -1,21 +1,36 @@
 import os
 import tensorflow as tf
 from utils import get_data, data_hparams
+from keras.callbacks import ModelCheckpoint
 
 
 # 0.准备训练所需数据------------------------------
 data_args = data_hparams()
 data_args.data_type = 'train'
-data_args.data_path = 'data/'
+data_args.data_path = '../dataset/'
 data_args.thchs30 = True
-data_args.aishell = False
-data_args.prime = False
-data_args.stcmd = False
-data_args.batch_size = 1
+data_args.aishell = True
+data_args.prime = True
+data_args.stcmd = True
+data_args.batch_size = 4
 data_args.data_length = 10
+# data_args.data_length = None
 data_args.shuffle = True
 train_data = get_data(data_args)
 
+# 0.准备验证所需数据------------------------------
+data_args = data_hparams()
+data_args.data_type = 'dev'
+data_args.data_path = '../dataset/'
+data_args.thchs30 = True
+data_args.aishell = True
+data_args.prime = False
+data_args.stcmd = False
+data_args.batch_size = 4
+# data_args.data_length = None
+data_args.data_length = 10
+data_args.shuffle = True
+dev_data = get_data(data_args)
 
 # 1.声学模型训练-----------------------------------
 from model_speech.cnn_ctc import Am, am_hparams
@@ -28,17 +43,27 @@ am = Am(am_args)
 
 if os.path.exists('logs_am/model.h5'):
     print('load acoustic model...')
-    am.ctc_model.load_weights('logs_am/model.h5', by_name=True)
+    am.ctc_model.load_weights('logs_am/model.h5')
 
 epochs = 10
 batch_num = len(train_data.wav_lst) // train_data.batch_size
 
-for k in range(epochs):
-    print('this is the', k+1, 'th epochs trainning !!!')
-    batch = train_data.get_am_batch()
-    am.ctc_model.fit_generator(batch, steps_per_epoch=batch_num, epochs=1)
+# checkpoint
+ckpt = "model_{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(os.path.join('./checkpoint', ckpt), monitor='val_acc', save_weights_only=False, verbose=1, save_best_only=True)
 
-am.ctc_model.save_weights('logs_am/model.h5', by_name=True)
+#
+# for k in range(epochs):
+#     print('this is the', k+1, 'th epochs trainning !!!')
+#     batch = train_data.get_am_batch()
+#     dev_batch = dev_data.get_am_batch()
+#     am.ctc_model.fit_generator(batch, steps_per_epoch=batch_num, epochs=10, callbacks=[checkpoint], workers=1, use_multiprocessing=False, validation_data=dev_batch, validation_steps=200)
+
+batch = train_data.get_am_batch()
+dev_batch = dev_data.get_am_batch()
+
+am.ctc_model.fit_generator(batch, steps_per_epoch=batch_num, epochs=10, callbacks=[checkpoint], workers=1, use_multiprocessing=False, validation_data=dev_batch, validation_steps=200)
+am.ctc_model.save_weights('logs_am/model.h5')
 
 
 # 2.语言模型训练-------------------------------------------
